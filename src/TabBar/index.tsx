01d1e34da6bf82387b3187584e171899a89a331c
Daazzer <w513765485@163.com>
2024-01-15 01:15:24 +0800

    feat: 添加 tab-bar-panel 交互

--- src/TabBar/index.tsx
+++ src/TabBar/index.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+import cartoonImg from '../assets/cartoon.jpg';
+import foodImg from '../assets/food.jpg';
+import movieImg from '../assets/movie.png';
+import lifeImg from '../assets/life.jpg';
 import './index.scss';
 
 type Tab = {
@@ -10,34 +16,52 @@ const tabs: Tab[] = [
   {
     label: '卡通',
     key: 'cartoon',
-    img: ''
+    img: cartoonImg
   },
   {
     label: '美食',
     key: 'food',
-    img: ''
+    img: foodImg
   },
   {
     label: '电影',
     key: 'movie',
-    img: ''
+    img: movieImg
   },
   {
     label: '生活',
     key: 'life',
-    img: ''
+    img: lifeImg
   }
 ];
 
 export default function TabBar() {
+  const [activeTab, setActiveTab] = useState<string>(tabs[0].key);
+
   return (
-    <ul className="tab-bar">
-      {tabs.map(tab =>
-        <li
-          className="tab-bar__tab"
-          key={tab.key}
-        >{tab.label}</li>
+    <div className="tab-bar">
+      <ul className="tab-bar-list">
+        {tabs.map(item =>
+          <li
+            className={classNames(
+              'tab-bar-list__item',
+              { active: activeTab === item.key }
+            )}
+            key={item.key}
+            onClick={() => setActiveTab(item.key)}
+          >{item.label}</li>
+        )}
+      </ul>
+      {tabs.map(item =>
+        <section key={item.key} className="tab-bar-panel">
+          <h2 className="tab-bar-panel__title">{item.label}</h2>
+          <img
+            className="tab-bar-panel__image"
+            src={item.img}
+            alt={item.key}
+          />
+        </section>
       )}
-    </ul>
+    </div>
   );
 };
