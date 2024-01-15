15b18d48aad7cd8c4d6e77596e29d799cb11f4a6
Daazzer <w513765485@163.com>
2024-01-15 16:52:12 +0800

    feat: 完成 TabBar 交互

--- src/TabBar/index.tsx
+++ src/TabBar/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import cartoonImg from '../assets/cartoon.jpg';
 import foodImg from '../assets/food.jpg';
@@ -37,9 +37,47 @@ const tabs: Tab[] = [
 
 export default function TabBar() {
   const [activeTab, setActiveTab] = useState<string>(tabs[0].key);
+  const [isFixed, setIsFixed] = useState(false);
+  const tabBarRef = useRef<HTMLDivElement>(null);
+  const tabBarPanelRefs: Array<HTMLElement | null> = [];
+
+  const onScroll = () => {
+    const tabBar = tabBarRef.current as HTMLDivElement;
+    const tabBarRect = tabBar.getBoundingClientRect();
+    const tabBarTop = tabBarRect.top;
+    const isFixed = tabBarTop <= 0;
+    setIsFixed(isFixed);
+
+    const tabBarPanel = (tabBarPanelRefs as any).findLast((item: HTMLElement) => {
+      const { top } = item.getBoundingClientRect() || {};
+      return top <= 67;
+    });
+
+    const activeTab = tabBarPanel?.dataset.key || '';
+
+    if (activeTab) {
+      setActiveTab(activeTab);
+    }
+  };
+
+  const onTabBarListItemClick = (key: string) => {
+    setActiveTab(key);
+    const tabBarPanel = (tabBarPanelRefs as HTMLElement[]).find((item: HTMLElement) => item.dataset.key === key);
+    if (tabBarPanel) {
+      tabBarPanel.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  });
 
   return (
-    <div className="tab-bar">
+    <div
+      ref={tabBarRef}
+      className={classNames('tab-bar', { fixed: isFixed })}
+    >
       <ul className="tab-bar-list">
         {tabs.map(item =>
           <li
@@ -48,12 +86,17 @@ export default function TabBar() {
               { active: activeTab === item.key }
             )}
             key={item.key}
-            onClick={() => setActiveTab(item.key)}
+            onClick={() => onTabBarListItemClick(item.key)}
           >{item.label}</li>
         )}
       </ul>
       {tabs.map(item =>
-        <section key={item.key} className="tab-bar-panel">
+        <section
+          key={item.key}
+          className="tab-bar-panel"
+          data-key={item.key}
+          ref={el => tabBarPanelRefs.push(el)}
+        >
           <h2 className="tab-bar-panel__title">{item.label}</h2>
           <img
             className="tab-bar-panel__image"
